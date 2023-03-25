42ce3bf29e721ec0654511b78e0d8fad004fb970
SharpeimQ <SharpHaiyan@gmail.com>
2023-03-25 16:27:40 -0400

    Add algorithm to select word from dictionary

--- index.rb
+++ index.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 dictionary = File.open('google-10000-english-no-swears.txt')
-dictionary = dictionary.read
+dictionary = dictionary.read.split.select { |word| word.length.between?(5, 12) }
 
-puts dictionary
+# Game Algorithm for Hangman
+class Hangman
+  def initialize(dictionary)
+    @dictionary = dictionary
+    @unknown_term = dictionary.sample
+    puts @unknown_term
+  end
+end
+
+Hangman.new(dictionary)
