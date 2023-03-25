7ce05d919a3dab216f48dbd5a2ff77bf4a9d9cd3
SharpeimQ <SharpHaiyan@gmail.com>
2023-03-25 17:00:14 -0400

    Build display for hangman game

--- index.rb
+++ index.rb
@@ -5,11 +5,40 @@ dictionary = dictionary.read.split.select { |word| word.length.between?(5, 12) }
 
 # Game Algorithm for Hangman
 class Hangman
+  # Random term when object is initialized
   def initialize(dictionary)
     @dictionary = dictionary
-    @unknown_term = dictionary.sample
-    puts @unknown_term
+    @secret_term = dictionary.sample
+    puts @secret_term
+  end
+
+  def game
+    puts 'Welcome to Hangman! Guess the Missing Term!'
+    status_display
+    term_display
+  end
+
+  def term_display
+    guessing_platform = []
+    @secret_term.length.times do
+      guessing_platform << '_'
+    end
+    puts guessing_platform.join(' ').inspect
+    insert_dashes
+  end
+
+  def status_display
+    guessed_terms = []
+    lives_left = 7
+    puts "You Have #{lives_left} Lives Left!"
+    puts "Guessed:#{guessed_terms}"
+    insert_dashes
+  end
+
+  def insert_dashes
+    puts '-------------------------------------------'
+    puts '-------------------------------------------'
   end
 end
 
-Hangman.new(dictionary)
+Hangman.new(dictionary).game
